b3a24b2f5bf1e341b546886b145b0efa5a1bdfeb
Santorinie <tnbendu@gmail.com>
2022-08-08 23:35:19 +0200

    Initial commit + added Dockerfile, docker-compose

---
+++ Adevinta-hw/Program.cs
@@ -0,0 +1,23 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// API healthcheck
+builder.Services.AddHealthChecks();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+// Map healthcheck to /health route
+app.MapHealthChecks("/health");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
